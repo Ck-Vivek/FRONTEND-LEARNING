e132b2127f7487aa1f78fd690dad56e6bca8c64e
Ck Vivek <ckvivek5695@gmail.com>
2025-10-16 10:09:16 +0530

    Add files via upload
    react changes

---
+++ my-reactapp/src/App.jsx
@@ -0,0 +1,14 @@
+import Header  from "./components/Header";
+
+const App=()=>{
+return(
+  <>
+  <Header/>{" "}
+    <div>
+        <h1>hello world</h1>
+        <p> I am learning react</p>
+    </div>
+    </>
+);
+}
+export default App;
---
+++ my-reactapp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header=( )=>{
+    return(
+        <>
+        <h1>This os my header</h1>
+        <p>This is a paragraph</p>
+        </>
+    );
+}
+export default Header;
